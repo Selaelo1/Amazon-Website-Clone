653950696183f0e8340248bdb81e8d075dae6bf5
Selaelo1 <selaelolanga10@gmail.com>
2024-08-19 13:29:32 +0200

    Update checkout.js

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -100,11 +100,15 @@ document.querySelectorAll('.js-delete-link')
     });
   });
 
+ function updateCartQuantity() {
   let cartQuantity = 0;
 
-cart.forEach((cartItem) => {
-  cartQuantity += cartItem.quantity;
-});
 
-document.querySelector('.js-return-to-home-link')
-  .innerHTML = `${cartQuantity} items`;
+ cart.forEach((cartItem) => {
+    cartQuantity += cartItem.quantity;
+  });
+
+  document.querySelector('.js-return-to-home-link')
+    .innerHTML = `${cartQuantity} items`;
+}
+updateCartQuantity();
